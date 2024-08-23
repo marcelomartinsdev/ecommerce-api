7bd6e893f455e0ed87716240cbfe8437a57e5ad5
Marcelo Martins <marcelomartinsdev@gmail.com>
2024-08-23 09:40:58 -0300

    feat: adicionar entidade Endereco e atualizar EcommerceApplication
    - Implementa a entidade Endereco com atributos para a gestão de endereços.
    - Atualiza EcommerceApplication e EcommerceApplicationTests para incluir as configurações relacionadas à nova entidade.

---
+++ src/main/java/com/api/ecommerce/model/Endereco.java
@@ -0,0 +1,25 @@
+package com.api.ecommerce.model;
+
+import com.api.ecommerce.enums.TipoEnderecoEnum;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class Endereco extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "clienteId")
+    private Cliente cliente;
+    private String logradouro;
+    private int numero;
+    private String complemento;
+    private String cidade;
+    private String estado;
+    private String cep;
+    private TipoEnderecoEnum tipoEnderecoEnum;
+}
