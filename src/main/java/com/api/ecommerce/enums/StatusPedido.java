16eec97897ea67d563e61d3b4737d40753f4d62f
Marcelo Martins <marcelomartinsdev@gmail.com>
2024-08-23 09:44:54 -0300

    feat: adicionar entidade Pedido e suas dependências
    - Implementa a entidade Pedido com atributos essenciais para a gestão de pedidos.
    - Adiciona o enum StatusPedido para gerenciar as fases do pedido.

---
+++ src/main/java/com/api/ecommerce/enums/StatusPedido.java
@@ -0,0 +1,18 @@
+package com.api.ecommerce.enums;
+
+public enum StatusPedido {
+    EM_ATEDIMENTO(1, "Em atedimento"),
+    AGURADANDO_CONFIMACO_PAGAMENTO(2, "Aguardando confimação pagamento"),
+    CANCELADO(3, "Cancelado"),
+    EXPEDIDO(4, "Expedido"),
+    ENTREGUE(5, "Entregue"),
+    AGUARDANDO_EXPEDICAO(6, "Aguardando expedição");
+
+    private Integer codigo;
+    private String descricao;
+
+    StatusPedido(Integer codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+}
